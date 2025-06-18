073af094df703e31924a0e67c338794fd0be1f03
Anthonypradosolis <apradosolis@danielcastelao.org>
2025-06-18 14:53:26 +0200

    traduccion

--- lib/util/constants/text_strings.dart
+++ lib/util/constants/text_strings.dart
@@ -2,71 +2,72 @@ class TTexts {
   // Global Texts
 
   // Onboarding Texts
-  static const String onBoardingTitle1 = "Choose your product";
-  static const String onBoardingTitle2 = "Select Payment Method";
-  static const String onBoardingTitle3 = "Deliver at your doorstep";
+  static const String onBoardingTitle1 = "Elige tu producto";
+  static const String onBoardingTitle2 = "Selecciona el método de pago";
+  static const String onBoardingTitle3 = "Entrega en tu puerta";
 
   static const String onBoardingSubTitle1 =
-      "Welcome to a World of Limitless Choices - Your Perfect Product Awaits!";
+      "¡Bienvenido a un mundo de opciones ilimitadas - Tu producto perfecto te espera!";
 
   static const String onBoardingSubTitle2 =
-      "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!";
+      "Para transacciones sin complicaciones, elige tu forma de pago - ¡Tu comodidad es nuestra prioridad!";
 
   static const String onBoardingSubTitle3 =
-      "From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery!";
+      "De nuestra puerta a la tuya - ¡Entrega rápida, segura y sin contacto!";
 
   // Home
-  static const String homeAppBarTitle = "Good day for your views ";
+  static const String homeAppBarTitle = "Buen día para tus vistas";
   static const String homeAppBarSubTitle = "Aitire";
 
   // Authentication Form Text
-  static const String firstName = "First Name";
-  static const String lastName = "Last Name";
-  static const String email = "E-Mail";
-  static const String password = "Password";
-  static const String newPassword = "New Password ";
-  static const String username = "Username ";
-  static const String phoneNo = "Phone Number ";
-  static const String rememberMe = "Remember Me ";
-  static const String forgetPassword = "Forget Password? ";
-  static const String signIn = "Sign In ";
-  static const String createAccount = "Create Account ";
-  static const String orSignInWith = "or sign in with ";
-  static const String orSignUpWith = "or sign up with ";
-  static const String iAgreeTo = "I agree to ";
-  static const String privacyPolicy = "Privacy Policy";
-  static const String termsOfUse = "Terms of use";
-  static const String verificationCode = "verificationCode ";
-  static const String resendEmail = "Resend Email ";
-  static const String resentEmailIn = "Resent email in ";
-  static const String and = " and ";
-  static const String name = "Name ";
-  static const String steam_id = "Steam ID ";
-  static const String PSN_user = "PSN User ";
+  static const String firstName = "Nombre";
+  static const String lastName = "Apellido";
+  static const String email = "Correo electrónico";
+  static const String password = "Contraseña";
+  static const String newPassword = "Nueva contraseña";
+  static const String username = "Nombre de usuario";
+  static const String phoneNo = "Número de teléfono";
+  static const String rememberMe = "Recuérdame";
+  static const String forgetPassword = "¿Olvidaste tu contraseña?";
+  static const String signIn = "Iniciar sesión";
+  static const String createAccount = "Crear cuenta";
+  static const String orSignInWith = "o inicia sesión con";
+  static const String orSignUpWith = "o regístrate con";
+  static const String iAgreeTo = "Acepto la";
+  static const String privacyPolicy = "Política de privacidad";
+  static const String termsOfUse = "Términos de uso";
+  static const String verificationCode = "Código de verificación";
+  static const String resendEmail = "Reenviar correo";
+  static const String resentEmailIn = "Correo reenviado en";
+  static const String and = " y ";
+  static const String name = "Nombre";
+  static const String steam_id = "ID de Steam";
+  static const String PSN_user = "Usuario de PSN";
 
   // Login
-  static const String loginTitle = "Welcome back!";
+  static const String loginTitle = "¡Bienvenido de nuevo!";
   static const String loginSubTitle =
-      "Discover Limitless Choices and Unmatched Convenience";
-  static const String signupTitle = "Create Your Account";
-  static const String forgetPasswordTittle = "Forgot Your Password?";
+      "Descubre opciones ilimitadas y una comodidad incomparable";
+  static const String signupTitle = "Crea tu cuenta";
+  static const String forgetPasswordTittle = "¿Olvidaste tu contraseña?";
   static const String forgetPasswordSubtitle =
-      "Don't worry! Reset your password quickly and securely.";
-  static const String changeYourPasswordTitle = "Password Reset Email Sent";
+      "¡No te preocupes! Restablece tu contraseña rápida y seguramente.";
+  static const String changeYourPasswordTitle =
+      "Correo de restablecimiento enviado";
   static const String changeYourPasswordSubTitle =
-      "Your Account Security is Our Priority! We've Sent You a Secure Link to Safely Change Your Password and Keep Your Account Protected ";
-  static const String confirmEmail = "Confirm Your Email";
+      "¡La seguridad de tu cuenta es nuestra prioridad! Te hemos enviado un enlace seguro para cambiar tu contraseña y proteger tu cuenta";
+  static const String confirmEmail = "Confirma tu correo electrónico";
   static const String confirmEmailSubTitle =
-      "Check your inbox to verify your email address. If you don't see it, please check your spam or junk folder. Verifying your email ensures the security of your account and allows you to access all features seamlessly.";
+      "Revisa tu bandeja de entrada para verificar tu dirección de correo. Si no lo ves, revisa la carpeta de spam o correo no deseado. Verificar tu correo asegura la seguridad de tu cuenta y el acceso completo a todas las funciones.";
   static const String emailNotReceiveMessage =
-      "Didn't receive the email? Check your spam folder or resend.";
-  static const String yourAccountCreatedTitle = "Account Created Successfully!";
+      "¿No recibiste el correo? Revisa tu carpeta de spam o vuelve a enviarlo.";
+  static const String yourAccountCreatedTitle = "¡Cuenta creada con éxito!";
   static const String yourAccountCreatedSubTitle =
-      "Welcome aboard! Start exploring your new account.";
+      "¡Bienvenido a bordo! Comienza a explorar tu nueva cuenta.";
 
-  static const String tContinue = "Continue";
-  static const String tResendEmail = "Resend Email";
+  static const String tContinue = "Continuar";
+  static const String tResendEmail = "Reenviar correo";
 
-  static const String submit = "Submit";
-  static const String done = "Done";
+  static const String submit = "Enviar";
+  static const String done = "Hecho";
 }
